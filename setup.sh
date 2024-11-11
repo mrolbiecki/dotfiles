7a40e26b22e9c8e046aa297ce895f343cb0c5ac6
Marcin Rolbiecki <rolbieckidev@gmail.com>
2024-11-11 17:17:33 +0100

    add initial dotfiles

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ~/dotfiles/setup.sh
+
+# List of all dotfile packages
+packages=("zsh" "oh-my-zsh" "geany" "git" "gnome")
+
+# Go to the dotfiles directory
+cd ~/dotfiles || exit
+
+# Run stow for each package
+for pkg in "${packages[@]}"; do
+    stow "$pkg"
+done
+
+echo "Dotfiles have been stowed successfully."
